860a2af334403d956ca4d7d2122a31360d09633d
powerfulyang <i@powerfulyang.com>
2024-11-01 18:37:11 +0800

    feat: discord oauth provider

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,4 +1,5 @@
 import app from '@/server'
+import { discordAuth } from '@hono/oauth-providers/discord'
 import { googleAuth } from '@hono/oauth-providers/google'
 
 app.get(
@@ -7,13 +8,24 @@ app.get(
     scope: ['email', 'profile'],
   }),
   (c) => {
-    const token = c.get('token')
-    const grantedScopes = c.get('granted-scopes')
     const user = c.get('user-google')
 
     return c.json({
-      token,
-      grantedScopes,
+      user,
+    })
+  },
+)
+
+app.get(
+  'auth/discord',
+  discordAuth({
+    scope: ['email', 'identify'],
+    redirect_uri: 'https://tunnel.littleeleven.com/api/auth/discord',
+  }),
+  (c) => {
+    const user = c.get('user-discord')
+
+    return c.json({
       user,
     })
   },
